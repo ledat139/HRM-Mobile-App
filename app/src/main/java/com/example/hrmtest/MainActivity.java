63cd3fe3c2ff40ada892f090bd77b081e60801b8
ledat139 <tiendatcntt1307@gmail.com>
2024-10-22 22:22:11 +0700

    first commit

---
+++ app/src/main/java/com/example/hrmtest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hrmtest;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.account);
+    }
+}
