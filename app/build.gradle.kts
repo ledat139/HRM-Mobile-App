5c4c977402ae60d7f619cd79d384b52c3be283cf
danglinh152 <danglinh.k4@gmail.com>
2025-01-01 00:19:20 +0700

    fix chảt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     implementation("com.sun.mail:android-activation:1.6.2")
     implementation("com.sun.mail:android-mail:1.6.2")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 }
